10bbc9cc4a633e9c759c58ecd96c309cc8e39d31
Hadeed Ahmad <hadeedji@gmail.com>
2024-07-01 20:45:44 +0500

    Finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,38 @@
 export default {
   content: ["index.html"],
   theme: {
-    extend: {},
+    colors: {
+      "dark-grey": "#1F1F1F",
+      "neon-green": "#C4F82A",
+      black: "#141414",
+      grey: "#333333",
+      white: "#FFFFFF",
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    fontSize: {
+      heading: [
+        "1.5rem",
+        {
+          lineHeight: "150%",
+          fontWeight: "600",
+        },
+      ],
+      "body-regular": [
+        "0.875rem",
+        {
+          lineHeight: "150%",
+          fontWeight: "400",
+        },
+      ],
+      "body-bold": [
+        "0.875rem",
+        {
+          lineHeight: "150%",
+          fontWeight: "700",
+        },
+      ],
+    },
   },
-  plugins: [],
 };
